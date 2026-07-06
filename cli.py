e7622c0b3bfc75228743e7a5f02dd6f52c6dccb3
ditriy <dimazay700@gmail.com>
2026-07-06 16:16:01 +0500

    init

---
+++ cli.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def parse_config_path() -> Path:
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "command",
+        choices=["run"],
+    )
+
+    parser.add_argument(
+        "--config",
+        type=Path,
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    return args.config
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from pathlib import Path
+
+from cli import parse_config_path
+from config import read_yaml
+
+
 def main() -> None:
-    pass
+    config_path = parse_config_path()
+    config = read_yaml(config_path)
+
+
+
+    match ...:
+        case _:
+            ...
+
+
 
 
 if __name__ == '__main__':
