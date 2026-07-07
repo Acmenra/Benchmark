0c2a85202775568c8b0237ac10e0e34cffaf8e74
ditriy <dimazay700@gmail.com>
2026-07-07 21:06:12 +0500

    [collector] чистка кода

--- hardware/collector.py
+++ hardware/collector.py
@@ -1,23 +1,25 @@
-from dataclasses import dataclass
-
 from config import SystemInfoConfig
-from hardware.cpu import collect_cpu
-
-
-# типы атрибутов тоже датаклассы (или нет), пример: config.py
-@dataclass(slots=True, frozen=True)
-class SystemInfo:
-    # cpu: ... | None
-    # gpu: ... | None 
-    # temperature: ... | None
-    # power: ...| None
-    # os: ... | None
-    ...
+from hardware.collectors.cpu import collect_cpu
+from hardware.entities import SystemInfo
+from hardware.enums import HardwarePlatform
+from hardware.collectors.gpu import collect_gpu
+from hardware.collectors.operating_system import collect_os
 
 
 class HardwareCollector:
-    def __init__(self, config: SystemInfoConfig) -> None:
-        ...
+    def __init__(self, system_info_config: SystemInfoConfig) -> None:
+        self.system_info_config = system_info_config
 
     def get_system_info(self) -> SystemInfo:
+        cpu = collect_cpu() if ... else None
+        gpu = collect_gpu() if self.system_info_config.collect_gpu else None
+        os = collect_os() if ... else None
         ...
+
+        return SystemInfo(
+            HardwarePlatform.UNKNOWN,
+            '',
+            cpu=cpu,
+            gpu=gpu,
+            os=os,
+        )
