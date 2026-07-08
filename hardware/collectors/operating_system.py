69500d8da6055baf68482e476d3918ef1a33b3ed
ditriy <dimazay700@gmail.com>
2026-07-08 16:27:50 +0500

    добавил collect_os(), который стерся при мердже по ошибке

--- hardware/collectors/operating_system.py
+++ hardware/collectors/operating_system.py
@@ -1,10 +1,25 @@
 # hardware/collectors/operating_system.py
 
+import platform
+
 from hardware.entities import OSInfo
 
 
-# смотреть hardware.entities.py,
-# примерная реализация лежит в hardware/CHECK_THIS.PY
-
 def collect_os() -> OSInfo:
-    ...
+    """
+    Сбор базовой информации об операционной системе.
+    Набор полей зависит от текущего OSInfo.
+    """
+    return OSInfo(
+        # После согласования entities.py можно добавить в OSInfo python_version
+        system=_empty_to_none(platform.system()),
+        release=_empty_to_none(platform.release()),
+        kernel=_empty_to_none(platform.version()),
+        architecture=_empty_to_none(platform.machine()),
+    )
+
+
+def _empty_to_none(value: str) -> str | None:
+    """Преобразование пустой строки в None для отчета."""
+    stripped_value = value.strip()
+    return stripped_value or None
